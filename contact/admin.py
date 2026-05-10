3a46f4533b5c931b8eb81df6102a574199f4ed28
Douglas Dugato <douglasdugato@gmail.com>
2026-05-10 15:52:42 -0300

    Adicionado admin/models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone',
+    list_filter = 'created_date',
+    search_fields = 'first_name', 'last_name',
