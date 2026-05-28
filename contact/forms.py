69cb36474594d27dd52a59b694702f806186e941
Douglas Dugato <douglasdugato@gmail.com>
2026-05-28 20:18:59 -0300

    fix: improve form validation logic

--- contact/forms.py
+++ contact/forms.py
@@ -32,22 +32,23 @@ class ContactForm(forms.ModelForm):
         fields = ('first_name', 'last_name',)
 
     def clean(self):
-        cleaned_data = self.cleaned_data
+        cleaned_data = super().clean()
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
 
         if first_name == last_name:
             msg = ValidationError('Nome não pode ser igual ao sobrenome', code='invalid')    
             self.add_error('last_name',msg)
+        return cleaned_data    
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
-        if any(char.isdigit() for char in first_name):
+        if first_name and any(char.isdigit() for char in first_name):
             raise ValidationError('Não pode possuir números', code='invalid')
         return first_name
     
     def clean_last_name(self):
         last_name = self.cleaned_data.get('last_name')
-        if any(char.isdigit() for char in last_name):
+        if last_name and any(char.isdigit() for char in last_name):
             raise ValidationError('Não pode possuir números', code='invalid')
         return last_name
